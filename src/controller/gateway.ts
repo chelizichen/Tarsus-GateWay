a659136901606326e5eb538546f69e661c18e5e2
chelizichen <1347290221@qq.com>
2023-03-09 09:44:37 +0800

    统一微服务请求格式

--- src/controller/gateway.ts
+++ src/controller/gateway.ts
@@ -1,29 +1,28 @@
-import { Request,Response } from 'express';
+import { Request, Response } from "express";
 import { Controller, Proxy, TarsusProxyService } from "tarsus";
 
 @Controller("/gateway")
 class GateWayController {
-
-    // 代理 微服务
-    // {
-    //     "interFace": 接口名,
-    //     "method": 方法名,
-    //     "data": 数据
-    //     "timeout": 超时时间,
-    //     "key": 对应后台服务名称
-    // }
+  // 代理 微服务
+  // {
+  //     "interFace": 接口名,
+  //     "method": 方法名,
+  //     "data": 数据
+  //     "timeout": 超时时间,
+  //     "proxy": 远程端口服务名称
+  // }
   @Proxy("/tarususRpc")
   public tarsusRpcProxy(req: Request, res: Response) {
     TarsusProxyService.transmit(req, res);
   }
-    // proxy, data, url, method
-    // 代理 Http服务
-    // {
-      // proxy: 远程端口服务名称
-      // method: 请求方法
-      // url:请求路由
-      // data:方法体
-    // }
+  // proxy, data, url, method
+  // 代理 Http服务
+  // {
+  //      proxy: 远程端口服务名称
+  //      method: 请求方法
+  //      url:请求路由
+  //      data:方法体
+  // }
   @Proxy("/tarsusHttp")
   public tarsusHttpProxy(req: Request, res: Response) {
     TarsusProxyService.request(req, res);
