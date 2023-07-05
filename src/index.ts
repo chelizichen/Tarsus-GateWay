5a84537bbff0c09e0d3719d48a83ee6db09947d5
chelizichen <1347290221@qq.com>
2023-07-05 23:23:17 +0800

    release cli-1.0.6 fixed bug

--- src/index.ts
+++ src/index.ts
@@ -14,20 +14,22 @@ import path from 'path'
 class TestApplication {
   static main(): void {
     loadGlobalPipe([LogGlobalPipe]);
-    loadController([GateWayController,ProjectController]);
-    loadInit((app)=>{
+    loadController([GateWayController, ProjectController]);
+    loadInit((app) => {
+      const { loadWebpackDev } = require('tarsus-cli');
       
-      app.use(express.static("dist/app"));
-    
-      app.get("*", (_req, res) => {
-        res.sendFile(path.join(__dirname, "app/index.html"));
-      });
+      loadWebpackDev(app)
+      // app.use(express.static("dist/app"));
+
+      // app.get("*", (_req, res) => {
+      //   res.sendFile(path.join(__dirname, "app/index.html"));
+      // });
 
     })
     // ms true 
     // 代表开启微服务网关的代理，所有请求将会转发给 后台微服务
-    loadServer({ms: true});
-    
+    loadServer({ ms: false });
+
   }
 }
 
