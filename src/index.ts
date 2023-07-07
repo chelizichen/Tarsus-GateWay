b264d95a66ea568739369751bdb0cfb8af21869c
chelizichen <1347290221@qq.com>
2023-07-07 15:10:54 +0800

    optimize : code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import express from 'express';
 import {
   TarsusHttpApplication,
   loadController,
@@ -9,7 +8,6 @@ import {
 import { GateWayController } from "./controller/gateway";
 import { LogGlobalPipe } from "./pipe/log";
 import { ProjectController } from "./controller/project";
-import path from 'path'
 @TarsusHttpApplication
 class TestApplication {
   static main(): void {
@@ -18,12 +16,6 @@ class TestApplication {
     loadInit((app) => {
       const { loadWebpackDev } = require('tarsus-cli');
       loadWebpackDev(app)
-      // app.use(express.static("dist/app"));
-
-      // app.get("*", (_req, res) => {
-      //   res.sendFile(path.join(__dirname, "app/index.html"));
-      // });
-
     })
     // ms true 
     // 代表开启微服务网关的代理，所有请求将会转发给 后台微服务
