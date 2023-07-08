81878e8f64018f45e85a1025d7adb39bc039cdf2
chelizichen <1347290221@qq.com>
2023-07-09 01:22:50 +0800

    optimize: cli && loader

--- src/client/App.vue
+++ src/client/App.vue
@@ -11,6 +11,13 @@
 
 <script setup lang="ts">
 import Header from '@/components/header/index.vue'
+import TaroUser from './taro/TaroUser.taro';
+import { onMounted } from 'vue';
+
+onMounted(() => {
+  console.log(TaroUser);
+  
+})
 </script>
 
 <style scoped></style>
